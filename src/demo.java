d3f17d7071e9bb4be77cc9a9ef11bf8144c3ba4b
marianidae <113852530+marianidae@users.noreply.github.com>
2023-10-07 18:48:36 +0300

    Fundamentals

---
+++ src/demo.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.awt.SystemColor.text;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
